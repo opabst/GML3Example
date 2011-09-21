d5e337603ff72cb1bf3b95805ecf75adbd53cbf3
pabst <oliverpabst.gm@googlemail.com>
2011-09-21 11:53:58 +0200

    removed XSDSchema usage in Encoder

--- src/de/oliverpabst/gml/GML3EncodingTest.java
+++ src/de/oliverpabst/gml/GML3EncodingTest.java
@@ -2,11 +2,21 @@ package de.oliverpabst.gml;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 import javax.xml.namespace.QName;
 import javax.xml.transform.TransformerException;
 
 import org.eclipse.xsd.XSDSchema;
+
+import org.eclipse.xsd.util.XSDSchemaLocator;
 import org.geotools.feature.DefaultFeatureCollection;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
@@ -15,18 +25,32 @@ import org.geotools.gml3.v3_2.GMLConfiguration;
 import org.geotools.wfs.v1_0.WFS;
 import org.geotools.xml.Configuration;
 import org.geotools.xml.Encoder;
-import org.opengis.geometry.primitive.Point;
+import org.geotools.xml.SchemaLocationResolver;
+import org.geotools.xml.SchemaLocator;
+import org.geotools.xml.Schemas;
+import org.geotools.xml.XSD;
+import org.geotools.xs.XS;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.Schema;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
 
 import net.opengis.wfs.FeatureCollectionType;
 import net.opengis.wfs.WfsFactory;
 
 public class GML3EncodingTest {
+	static GeometryFactory gf = new GeometryFactory();
+	public static String NAMESPACE = "http://www.geotools.org/test";
+	public static QName TestFeature = new QName(NAMESPACE, "TestFeature");
+	
+	
 	
 	/**
 	 * @param args
@@ -41,14 +65,13 @@ public class GML3EncodingTest {
 	public static void encodeGML3() {
 		FeatureCollectionType fc = buildFeatureCollection();
 		Configuration config = new GMLConfiguration();
-		// TODO: Set up schema
-		XSDSchema schema = null;
-		Encoder encoder = new Encoder(config, schema);
+		
+		Encoder encoder = new Encoder(config);
 		ByteArrayOutputStream xml = new ByteArrayOutputStream();
 		// TODO: Define custom feature collection for given type
-		QName coll = new QName("http://www.dbs.uni-hannover.de/test", "TestCollection");
+		
 		try {
-			encoder.encode(fc, coll, xml);
+			encoder.encode(fc, TestFeature, xml);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -120,5 +143,4 @@ public class GML3EncodingTest {
         
         return fc;
 	}
-
 }
